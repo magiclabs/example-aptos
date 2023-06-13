a0110500bbe657dd393ac7ed61d944c803f0c48d
Jay Hwang <octave08b@gmail.com>
2023-06-13 13:00:03 +0900

    feat: update @magic-ext/aptos to 0.6.0 and add polyfill for encoding

--- examples/example-aptos-expo/src/pages/RootLayout.tsx
+++ examples/example-aptos-expo/src/pages/RootLayout.tsx
@@ -1,6 +1,10 @@
+import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+
 import { magic } from "@/lib/magic";
 import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+polyfillEncoding();
 
 export const RootLayout = () => {
   return (
