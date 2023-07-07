52afba9e5dfd9984377a8f212198995d439f6020
Jay Hwang <octave08b@gmail.com>
2023-07-08 07:56:44 +0900

    chore: add comment for the ref

--- examples/example-aptos-react/src/utils/parseOctasToApt.ts
+++ examples/example-aptos-react/src/utils/parseOctasToApt.ts
@@ -1,3 +1,4 @@
+// Note. https://github.com/aptos-labs/token/blob/aeef75178c76f6f50f919914d45f348bd55cbb7c/minting-tool/cli/src/utils.ts#L88
 export const parseOctasToApt = (amount: string): string => {
   return (Number.parseInt(amount, 10) / 100000000).toFixed(8);
 };
